b705371a0015c85e53b5cebd55ba3c5c4a666c83
Pramod Jodhani <mrpramodjodhani@gmail.com>
2021-06-02 18:37:56 +0530

    first commit

---
+++ iconic-woo-delivery-slots-compat-custom-shipping.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plugin Name:     WooCommerce Delivery Slots by Iconic [Custom Shipping Methods for WooCommerce]
+ * Plugin URI:      https://iconicwp.com/products/woocommerce-delivery-slots/
+ * Description:     Compatibility between WooCommerce Delivery Slots by Iconic and Custom Shipping Methods for WooCommerce by Tyche Softwares.
+ * Author:          Iconic
+ * Author URI:      https://iconicwp.com/
+ * Text Domain:     iconic-woo-delivery-slots-compat-custom-shipping-methods
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Iconic_Woo_Delivery_Slots_Compat_Custom_Shipping_Methods
+ */
+
+/**
+ * Is WooCommerce Custom Shipping Methods active?
+ *
+ * @return bool
+ */
+function iconic_compat_custom_shipping_is_active() {
+	return class_exists( 'Alg_WC_Custom_Shipping_Methods' );
+}
+
+/**
+ * Change the format of shipping method ID.
+ *
+ * @return array
+ */
+function iconic_compat_custom_shipping_replace_shipping_method_id( $shipping_method_options ) {
+	if ( ! iconic_compat_custom_shipping_is_active() ) {
+		return $shipping_method_options;
+	}
+
+	foreach ( $shipping_method_options as $key => $method_name ) {
+		if ( 'alg_custom:' === substr( $key, 0, 11 ) ) {
+			$parts       = explode( ':', $key );
+			$instance_id = $parts[1];
+			$new_key     = sprintf( 'alg_wc_shipping:%d', $instance_id );
+			unset( $shipping_method_options[ $key ] );
+			$shipping_method_options[ $new_key ] = $method_name;
+		}
+	}
+
+	return $shipping_method_options;
+}
+
+add_filter( 'iconic_wds_shipping_method_options', 'iconic_compat_custom_shipping_replace_shipping_method_id', 10 );
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Description
+
+This is a compatibility plugin for [WooCommerce Delivery Slots](https://iconicwp.com/products/woocommerce-delivery-slots/?utm_source=Iconic&utm_medium=Github&utm_campaign=iconic-woo-delivery-slots-compat-shipping-pro)  by Iconic and [Custom Shipping Methods for WooCommerce](https://wordpress.org/plugins/custom-shipping-methods-for-woocommerce/) by Tyche Softwares.
+
+# Changelog
+
+## 0.1.0 (2021-06-02)
+* Initial Release.
